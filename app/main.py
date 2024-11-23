c72fa593c3f89882e8beb80d03be3c4565c19618
Leganyst <Leganyst@yandex.ru>
2024-11-23 18:32:44 +0300

    fix bug in auth router

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ from app.routers.api.comments import router as comments_router
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
+        # await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
     yield
     await engine.dispose()
