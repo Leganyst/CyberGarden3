8d214521268138eda6f96e2a1937ff3f348f41f1
Leganyst <Leganyst@yandex.ru>
2024-11-23 19:09:31 +0300

    fix check tg data auth

--- app/routers/dependencies/auth.py
+++ app/routers/dependencies/auth.py
@@ -2,13 +2,17 @@ from hashlib import sha256
 import hmac
 from urllib.parse import parse_qsl
 
-def verify_telegram_data(data: dict, bot_token: str) -> bool:
+from app.schemas.telegram import TelegramAuthRequest
+
+def verify_telegram_data(data: TelegramAuthRequest, bot_token: str) -> bool:
     """
     Проверка подписи данных, переданных Telegram.
     """
-    auth_data = {k: v for k, v in data.items() if k != "hash"}
+    
+    auth_data = data.__dict__
+    auth_data = {k: v for k, v in auth_data.items() if k != "hash"}
     sorted_data = sorted(auth_data.items())
     data_check_string = "\n".join(f"{k}={v}" for k, v in sorted_data)
     secret_key = sha256(bot_token.encode()).digest()
     hmac_hash = hmac.new(secret_key, data_check_string.encode(), sha256).hexdigest()
-    return hmac_hash == data.get("hash")
+    return hmac_hash == auth_data.get("hash")
